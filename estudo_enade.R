be5014c16d1b6effcdb9ca3b0eaec0a523fd62d2
Emmanuel Gomes <aeag@outlook.com>
2020-10-28 22:02:25 -0300

    análise descritiva

--- estudo_enade.R
+++ estudo_enade.R
@@ -1,6 +1,6 @@
 #Criando um vetor com os pacotes a serem instalados e importados
 vetor_pacotes = c("readr", "ggplot2", "plotly", "e1071", "dplyr", "Hmisc", 
-                  "DescTools", "esquisse", "gridExtra", "devtools")
+                  "DescTools", "esquisse", "gridExtra", "devtools", "tidyverse")
 install.packages(vetor_pacotes)
 
 #Aplica o procedimento require à lista vetor_pacotes
@@ -15,6 +15,7 @@ library(Hmisc)
 library(DescTools)
 library(esquisse)
 library(gridExtra)
+library(psych)
 
 # Atribui e recupera o diretório de trabalho. Work Directory (Padrão Linux)
 setwd("~/Documentos/DataScienceFB/Estatistica/projeto-final-ds-estatisica")
@@ -34,6 +35,9 @@ enade_2017_csv = read_csv2("MICRODADOS_ENADE_2017.txt")
 enade_2017_mil_linhas = head(enade_2017_table, 1000)
 # write_csv(enade_2017_mil_linhas, "enade_2017_mil_linhas.csv")
 
+enade2017_filtrado= enade_2017_mil_linhas %>% filter(CO_GRUPO==2001 |CO_GRUPO==21|
+                                           CO_GRUPO==5710 |CO_GRUPO==5806)
+
 #Dimensões do data frame
 dim(enade_2017_mil_linhas)
 
@@ -50,3 +54,25 @@ enade_ti = mutate(enade_ti, CO_REGIAO_CURSO = case_when(CO_REGIAO_CURSO == 1 ~ "
                                                            CO_REGIAO_CURSO == 5 ~ "Centro-Oeste") )
 class(enade_ti$CO_GRUPO)
 class(enade_ti$CO_REGIAO_CURSO)
+s = summary(enade_2017_csv)
+d = describe(enade_2017_csv)
+
+enade_ti %>% select(CO_REGIAO_CURSO, NU_IDADE) %>% group_by(CO_REGIAO_CURSO) %>% summarise(media = mean(NU_IDADE, na.rm = T))
+enade2017_sem_NA = enade_2017_csv %>% na.omit()
+dim(enade2017_sem_NA)
+dim(enade_2017_csv)[1] - dim(enade2017_sem_NA)[1]
+fs = table(enade_filtrado$NU_IDADE)
+max(fs)
+nomes = names(fs)
+moda = nomes[fs == max(fs)]
+media = mean(enade_filtrado$NU_IDADE)
+mediana = median(enade_filtrado$NU_IDADE)
+moda = Mode(enade_filtrado$NU_IDADE)
+qtdade = length(enade_filtrado$NU_IDADE)
+consolidado = data.frame("Quantidade"=qtdade,
+                         "Média"=media,
+                         "Mediana"=mediana,
+                         "Moda"=moda)
+consolidado
+skewness(enade_filtrado$NU_IDADE)
+kurtosis(enade_2017_csv$NU_IDADE)
